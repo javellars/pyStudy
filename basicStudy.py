e18f413c7c9a41ed3e9314e800d689b4afe80fcd
Jullia <robas.jullia@gmail.com>
2024-08-31 21:35:25 -0300

    correção de comparação de idades

--- basicStudy.py
+++ basicStudy.py
@@ -2,22 +2,22 @@
 
 ##Meu nome é Jullia e tenho 21 anos, e você?
 nome_anfitriao = 'Jullia'
-idade = 21
-idade = str(idade) ##conversão de tipo
+idade_anfitriao = 21
+idade_anfitriao = str(idade_anfitriao) ##conversão de tipo
 
-print("Meu nome é " + nome_anfitriao + " e tenho " + idade + " anos, e você?")
+print("Meu nome é " + nome_anfitriao + " e tenho " + idade_anfitriao + " anos, e você?")
 
 ##aplicação de SLICE -> sempre lembrar que string é um vetor se char
 print(nome_anfitriao[0:6])
 print(nome_anfitriao[:: -1])
 
-nome = input('Qual o seu nome?: ')
-idade = input('Qual sua idade?: ')
-idade = str(idade)
+nome_convidado = input('Qual o seu nome?: ')
+idade_convidado = input('Qual sua idade?: ')
+idade_convidado = str(idade_convidado)
 
-print("Meu nome é " + nome + " e tenho " + idade + " anos, prazer!")
-print(nome[0:len(nome)])
-print(nome[::-1])
+print("Meu nome é " + nome_convidado + " e tenho " + idade_convidado + " anos, prazer!")
+print(nome_convidado[0:len(nome_convidado)])
+print(nome_convidado[::-1])
 
 data_nsc = input('Em que ano você nasceu?: ')
 ##print(type(data_nsc)) -> me dá o tipo do dado
@@ -25,17 +25,31 @@ data_nsc = input('Em que ano você nasceu?: ')
 data_nsc = int(data_nsc)
 ##print(type(data_nsc))
 
-idade = 2024 - data_nsc
+teste_idade = 2024 - data_nsc
+teste_idade = str(teste_idade)
 
-##aqui seria legal ter um If pra falar se fulano tem ou não a idade que falou
-print("UaU! Você tem mesmo " , idade)
+if  teste_idade == idade_convidado :
+    print("UaU! Você tem mesmo " , idade_convidado)
+else:
+    print("Você mentiu a idade")
+
+
+if idade_anfitriao == idade_convidado:
+    print('tem a mesma idade!')
+elif idade_anfitriao < idade_convidado:
+    print(nome_convidado + ' é mais velho que ' + nome_anfitriao)
+else:
+    print( nome_anfitriao + ' é mais velho que ' + nome_convidado)
 
 
 ##String formatada
-texto1 = f"Agora {nome_anfitriao} e {nome} já se conhecem!"
+texto1 = f"Agora {nome_anfitriao} e {nome_convidado} já se conhecem!"
 
 print (texto1)
 ##colocar um condicional pra falar se tem a mesma idade
+
+
+
 
 ##métodos para String
 ##print(mensagem.upper()) -> imprime todo o texto da mensagem em maiúsculo
@@ -45,3 +59,4 @@ print (texto1)
 ##print(mensagem.find('palavra')) -> me dá a posição que determinada palavra inicia [-1 significa que não achou]
 ##print(mensagem.replace('a', 'e')) -> substitui todo a por e; podemos utilizar com palavras também
 ##print(mensagem.strip()) -> remove os espaços que tem antes do primeiro caractere
+
