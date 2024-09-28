9c18fd78ae9acb3394fe800f937b1e9a522ee423
Jullia <robas.jullia@gmail.com>
2024-09-28 17:39:10 -0300

    calculadora map lambda com input

--- dataStructure/calculadoraMap.py
+++ dataStructure/calculadoraMap.py
@@ -10,4 +10,26 @@ print(list(listaMulti))
 listaDiv = map(lambda x,y: x/y, lista1, lista2)
 print(list(listaDiv))
 
-      
+## Agora com os inputs
+
+valorX = input('Digite o primeiro valor: ')
+valorY = input('Digite o segundo valor: ')
+sinal = input('Escolha * para multiplicação, / para divisão, + para soma e - para subtração: ')
+
+# tranformando assim conseguimos contornar o problema da concatenação na soma
+valorX = list(map(int, valorX))
+valorY = list(map(int, valorY))
+
+if sinal == '+':
+    listaSoma = map(lambda x,y: x + y,valorX, valorY)
+    print(list(listaSoma))
+if sinal == '-':
+    listaSub = map(lambda x,y: x - y,valorX, valorY)
+    print(list(listaSub))
+if sinal == '*':
+    listaMulti = map(lambda x,y: x * y,valorX, valorY)
+    print(list(listaMulti))
+if sinal == '/':
+    listaDiv = map(lambda x,y: x / y,valorX, valorY)
+    print(list(listaDiv))
+
